a75214dea3628182811f8a5dc26a4f3acfb43298
subhan633 <shakeelsubhan632@gmail.com>
2025-08-06 00:57:28 +0500

    first commit

---
+++ chatbot_backend.py
@@ -0,0 +1,40 @@
+import os
+from langgraph.graph import StateGraph, START, END
+from langchain_groq import ChatGroq
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+
+load_dotenv()
+
+groq_api_key = os.getenv("GROQ_API_KEY")
+
+# Initialize the Groq model
+model = ChatGroq(
+    api_key=groq_api_key,
+    model="llama-3.3-70b-versatile",
+    temperature=0.7,          # Adjust for response randomness
+)
+
+class ChatState(TypedDict):
+
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+
+    messages = state['messages']
+    response = model.invoke(messages)
+    return {'messages': [response]}
+
+# Checkpointer
+checkpointer = InMemorySaver()
+
+graph = StateGraph(ChatState)
+
+graph.add_node('chat_node', chat_node)
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+workflow = graph.compile(checkpointer=checkpointer)
